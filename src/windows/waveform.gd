cccbc74f9632e7056294a3f678dabb586961487e
slidick <slidick@thepeu.pw>
2025-05-06 20:50:28 -0400

    Fix: Increase performance of waveform rendering

--- src/windows/waveform.gd
+++ src/windows/waveform.gd
@@ -1,6 +1,9 @@
 extends Control
 
+const JUMP: int = 40
+
 var data: Array = []
+
 
 func setup(p_data: Array) -> void:
 	data = p_data
@@ -8,13 +11,11 @@ func setup(p_data: Array) -> void:
 
 
 func _draw() -> void:
-	for i in range(0, len(data)-1):
+	var vertices := []
+	for i in range(0, len(data)-1, JUMP):
 		var v1 := Vector2(
 			(float(i) / len(data)) * float(size.x),
 			(size.y / 2) + (data[i].x * (size.y/2)),
 		)
-		var v2 := Vector2(
-			(float(i+1) / len(data)) * float(size.x),
-			(size.y / 2) + (data[i+1].x * (size.y/2)),
-		)
-		draw_line(v1, v2, Color.WEB_GREEN, 1.0)
+		vertices.append(v1)
+	draw_polyline(vertices, Color.WEB_GREEN, 1.0, true)
