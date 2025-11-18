85731f7e0f4f2f1da2d55d46ddbc7947e68bb1e5
slidick <slidick@thepeu.pw>
2025-11-18 16:03:06 -0500

    Fix issue with canceling object/sfx drags

--- src/resources/sfx.gd
+++ src/resources/sfx.gd
@@ -168,7 +168,6 @@ class SFXNode2D extends Node2D:
 				dragging = false
 				drag_started = false
 				position = start_drag_position
-				get_viewport().set_input_as_handled()
 		if event is InputEventMouseButton:
 			if event.button_index == MOUSE_BUTTON_LEFT:
 				if not event.shift_pressed:
